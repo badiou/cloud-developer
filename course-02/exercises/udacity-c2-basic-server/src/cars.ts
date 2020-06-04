66d61bae6f876e4161003bf371adc89e1b515954
badiou <ourobadiou@gmail.com>
2020-06-04 10:33:10 +0000

    Deploying my App to the Elastic BeanStalk: http://udagram-best-badiou-dev.us-east-1.elasticbeanstalk.com/api/v0/feed

---
+++ course-02/exercises/udacity-c2-basic-server/src/cars.ts
@@ -0,0 +1,16 @@
+//ici on définit le modele de Car avec les types ....
+export interface Car {
+    make: string;
+    type: string;
+    model: string;
+    cost: number;
+    id: number;
+}
+
+//ici c'est la liste de Cars........
+export const cars: Car[] = [
+    { make: 'tesla', type: 'sedan', model: 'roadster', cost: 33, id: 0 },
+    { make: 'tesla', type: 'suv', model: 'model 3', cost: 48, id: 1 },
+    { make: 'toyota', type: 'sedan', model: 'prius', cost: 22, id: 2 },
+    { make: 'honda', type: 'sedan', model: 'civic', cost: 22, id: 3 }
+   ]
