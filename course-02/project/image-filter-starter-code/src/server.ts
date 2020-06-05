024dea865d467e1a76ad1e84accdc03364bc920b
badiou <ourobadiou@gmail.com>
2020-06-05 08:50:52 +0000

    Deploy the app with url http://image-filter-starter-code-dev22222222222222.us-east-1.elasticbeanstalk.com/

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,23 +13,15 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
-
-  //! END @TODO1
+  app.get('/filteredimage', async (req: express.Request, res: express.Response) => {
+    const { image_url: imageUrl } = req.query;
+    if (!imageUrl) {
+      return res.status(400).send({ auth: false, message: 'Image url is missing or malformed' });
+    }
+    const filteredPath = await filterImageFromURL(imageUrl);
+    res.sendFile(filteredPath, {}, () => deleteLocalFiles([filteredPath]));
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
