0d7d99a7400aac5674154a83f7dc058c61afa1df
badiou <ourobadiou@gmail.com>
2020-07-17 23:49:57 +0000

    set RS256 Auth0 certificate

--- course-04/project/c4-final-project/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project/backend/src/lambda/auth/auth0Authorizer.ts
@@ -5,7 +5,25 @@ import { JwtPayload } from '../../auth/JwtPayload';
 import { verify } from 'jsonwebtoken'
 
 
-const cert = ``
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDBTCCAe2gAwIBAgIJD/kR1YPyfPWSMA0GCSqGSIb3DQEBCwUAMCAxHjAcBgNV
+BAMTFWZzbmR0b2dvLmV1LmF1dGgwLmNvbTAeFw0yMDAyMTcwOTUwMzVaFw0zMzEw
+MjYwOTUwMzVaMCAxHjAcBgNVBAMTFWZzbmR0b2dvLmV1LmF1dGgwLmNvbTCCASIw
+DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANgirhMR1wghtX5uoW+o6PWw4oWY
+hn6E51BNkmqew8BOSNHaYt90lOxFHGos5G3pTKTewSLycWl0F7ZFdB2jSiAVqXmJ
++hC9sITq18RG+1oJyn9lPciJfWLiqLYI6+qxpJ3CSq7I8MiA4VmlIBUqam4pxp5g
+c/KepbObQb8qMdOx+lmpdbBe9VB4DSxqIlK/9MVf0LSPm8i/lnZw9QthMfd6n+oM
+sBKrdsR+l6VhkB2432WNHxLWrNZspRcN0DH/Rf73aqRGKor3gdIrDTcxEGF1HTin
+yTuTMRCf0iKx9dMMwPr6TnPs1qFuiOM7m1QgaTSzuapmbZzcN2kJrljh9KcCAwEA
+AaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUwNtnrW1nUKEOnMGv+TYa
+xTLbNt0wDgYDVR0PAQH/BAQDAgKEMA0GCSqGSIb3DQEBCwUAA4IBAQB6gg3F4agQ
+vLMaHc3mGqAbwhXAnvVvktPK6Ubst05pRJCLIFZgfgPaNIdF0jvXBrPuBAqmg+es
+3SmIShFFaX3pzRY+qXvyzJ8wi15uwlfkaFS16fup/PEJQEmr0KXe4u26qHe13ZxD
+pIfGI0DXWtirMG8qr8qE8LvZX88u43pq1TazQO/5N7Sp02mNeeTibvk4ELLbDF4q
+wzILJvFtWPyoa5d9rE1YbDrY1neUVAdeohEmNl8Zjl5pp/9ZxiiiCHU8cT8W8aAn
+PS7st0/uUxgvBOHgjFMmZwiplWDUGvAJ22/C3TwAaEaXbRwGPFfLcMxqoc0dSWj2
+53y1pUcryHMo
+-----END CERTIFICATE-----`
 
 export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   try {
