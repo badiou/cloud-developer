bfb0391d4e20b5317415af5bbcb76a936f3106c7
badiou <ourobadiou@gmail.com>
2020-07-19 13:56:32 +0000

    update deleteTodo

--- course-04/project/c4-final-project/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project/backend/src/lambda/http/deleteTodo.ts
@@ -1,30 +1,26 @@
-import 'source-map-support/register'
+import 'source-map-support/register';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda';
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import { deleteTodo } from '../../businessLogic/todos'
+import { deleteTodo } from '../../businessLogic/todos';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
- if(!(await deleteTodo(event))) {  
-   // deleteTodo return false because todo does not exist
-  return {
-    statusCode: 404,
-    body: JSON.stringify({
-    error: 'This todo does not exist'
+  
+  if (!(await deleteTodo(event))) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        error: 'Item does not exist'
       })
-    }
-  } // end of if
-  else{
-    return{
-      statusCode: 202,
-    body: JSON.stringify({
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true
-      },
-      body: JSON.stringify({})
-      })
-    }
-  }// end of else
-}
+    };
+  }
 
+  return {
+    statusCode: 202,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({})
+  };
+}
